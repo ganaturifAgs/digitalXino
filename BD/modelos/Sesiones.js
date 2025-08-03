22710340e2f22b930c61294878cafb98b1f9f791
ganaturifAgs <xino.cartographic.2025@gmail.com>
2025-08-03 11:04:56 -0600

    Corrigio el modelo de Sessiones

--- BD/modelos/Sesiones.js
+++ BD/modelos/Sesiones.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const sesionesSchema = new mongoose.Schema(
     {
         _id:{type:Number, require:true},
-        usuario:String,
+        usuario:{type:String, require:true},
         inicio:Date,
         ip:String,
-        ubicacion:{long:Number,lat:Number},
+        ubicacion:String,
         device:String
     })
 
